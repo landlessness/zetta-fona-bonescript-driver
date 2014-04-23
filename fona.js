c3caf6c8ab7cce7a5c301d9d9227ebe2d937ab4c
Brian Mulloy <brian@landlessness.net>
2014-04-23 18:56:56 -0400

    signal strength

--- fona.js
+++ fona.js
@@ -4,39 +4,45 @@ var util = require('util');
 var Fona = module.exports = function(serialPort) {
   Device.call(this);
 
+  this.smsMessages = [];
   this._serialPort = serialPort;
-  this.simCCID = null; // gets populated after data is parsed from device
-
+  
   var self = this;
   this._serialPort.on('data', function(data){
-    self.parseData(data);
+    self._parseATData(data);
   });
   
 };
 util.inherits(Fona, Device);
 
 Fona.prototype.init = function(config) {
+  
   config
   .name('Adafruit Fona')
   .type('fona')
-  .monitor('adcVoltage')
-  .monitor('batteryVoltage')
+  .monitor('receivedSignalStrengthDBM', {title: 'Received Signal Strength', units: 'dBm' })
+  .monitor('receivedSignalStrengthCondition')
   .monitor('batteryPercentage')
+  .monitor('smsCount')
+  .monitor('batteryVoltage')
+  .monitor('adcVoltage')
   .state('waiting')
-  .when('waiting', { allow: ['write', 'send-sms']})
+  .when('waiting', { allow: ['write', 'send-sms', 'read-sms']})
   .when('writing', { allow: [] })
   .map('write', this.write, [{ name: 'command', type: 'text'}])
+  .map('read-sms', this.readSMS, [
+    { name: 'index', title: 'Message Index', type: 'range',
+      min: 1, step: 1}])
   .map('send-sms', this.sendSMS, [
-    { name: 'phoneNumber', type: 'text'},
+    { name: 'phoneNumber', title: 'Phone Number to Send SMS', type: 'text'},
     { message: 'message', type: 'text'},
     ]);
   
+  this._requestVitals();
   var self = this;
   setInterval(function() {
-    self.requestADCVoltage();
-    self.requestBatteryPercentAndVoltage();
-    self.requestSIMCCID();
-  }, 10000);
+    self._requestVitals();
+  }, 5000);
   
 };
 
@@ -50,29 +56,99 @@ Fona.prototype.write = function(command, cb) {
 
 Fona.prototype.sendSMS = function(phoneNumber, message, cb) {
   this.state = 'sending-sms';
-  
   this._serialPort.write('AT+CMGF=1' + '\n\r');
   this._serialPort.write('AT+CMGS="' + phoneNumber + '"' + '\n\r');
   this._serialPort.write(message + '\n\r');
   this._serialPort.write([0x1a]);
-
   this.state = 'waiting';
-
   cb();
 };
 
-Fona.prototype.parseData = function(data) {
-  this.log('data received: ' + data);
+Fona.prototype.readSMS = function(index, cb) {
+  this.log('readSMS: ' + index);
+  this.call('write', 'AT+CMGF=1');
+  this.call('write', 'AT+CSDH=1');
+  this.call('write', 'AT+CMGR=' + index);
+  cb();
+}
+
+Fona.prototype._requestBatteryPercentAndVoltage = function() {
+  this.call('write', 'AT+CBC');
+}
+
+Fona.prototype._requestADCVoltage = function() {
+  this.call('write', 'AT+CADC?');
+}
+
+Fona.prototype._requestSIMCCID = function() {
+  this.call('write', 'AT+CCID');
+}
+
+Fona.prototype._requestSMSCountAndCapacity = function() {
+  this.call('write', 'AT+CMGF=1');
+  this.call('write', 'AT+CPMS?');
+}
+
+Fona.prototype._requestSMSCountAndCapacity = function() {
+  this.call('write', 'AT+CMGF=1');
+  this.call('write', 'AT+CPMS?');
+}
+
+Fona.prototype._requestRegistrationStatusAndAccessTechnology = function() {
+  this.call('write', 'AT+CREG?');
+}
+
+Fona.prototype._requestSignalQuality = function() {
+  this.call('write', 'AT+CSQ');
+}
+
+Fona.prototype._requestVitals = function(context) {
+  this._requestADCVoltage();
+  this._requestBatteryPercentAndVoltage();
+  this._requestSMSCountAndCapacity();
+  this._requestSIMCCID();
+  this._requestRegistrationStatusAndAccessTechnology();
+  this._requestSignalQuality();
+}
+
+Fona.prototype._parseATData = function(data) {
   var match = null;
+  this.log('parsing AT data: ' + data);
+  
   switch (true) {
     case !!(match = data.match(/^\+CADC: .*,(.*)$/)):
       this.adcVoltage = match[1];
+      break;
+    case !!(match = data.match(/^\+CREG: (.*),(.*)$/)):
+      this.registrationStatus = match[1];
+      this.accessTechnology = match[2];
+      break;
+    case !!(match = data.match(/^\+CSQ: (\d*),(\d*)$/)):
+      this.receivedSignalStrength = match[1];
+      this.receivedSignalStrengthDBM = this._receivedSignalStrengthIndicatorMap[match[1]]['dBm'];
+      this.receivedSignalStrengthCondition = this._receivedSignalStrengthIndicatorMap[match[1]]['condition'];
+      this.bitErrorRate = match[2];
       break;
     case !!(match = data.match(/^\+CBC: .*,(.*),(.*)$/)):
       this.batteryPercentage = match[1];
       this.batteryVoltage = match[2];
       break;
+    case !!(match = data.match(/^\+CPMS: "[A-Z_]+",(\d+),(\d+),.*$/)):
+      this.smsCount = match[1];
+      this.smsCapacity = match[2];
+      break;
+    case !!(match = data.match(/^\+CMGR: "([A-Z]+) ([A-Z]+)","([^"]*)","([^"]*)","([^"]*)",(\d+),(\d+),(\d+),(\d+),"([^"]*)",(\d+),(\d+).*$/)):
+      this.smsMessages.push(
+        {
+          receivedState: match[1],
+          readState: match[2],
+          sendersPhoneNumber: match[3],
+          timeStamp: match[5],
+        }
+      );
+      break;
     case !!(match = data.match(/^(\d[a-zA-Z0-9]*)$/)):
+      // keep check got SIM CCIDone last
       this.simCCID = match[1];
       break;
     default:
@@ -80,14 +156,63 @@ Fona.prototype.parseData = function(data) {
   }
 }
 
-Fona.prototype.requestBatteryPercentAndVoltage = function() {
-  this.call('write', 'AT+CBC');
-}
-
-Fona.prototype.requestADCVoltage = function() {
-  this.call('write', 'AT+CADC?');
-}
-
-Fona.prototype.requestSIMCCID = function() {
-  this.call('write', 'AT+CCID');
+Fona.prototype._receivedSignalStrengthIndicatorMap = {
+  2: {dBm: -109,
+    condition: 'Marginal'},
+  3: {dBm: -107,
+    condition: 'Marginal'},
+  4: {dBm: -105,
+    condition: 'Marginal'},
+  5: {dBm: -103,
+    condition: 'Marginal'},
+  6: {dBm: -101,
+    condition: 'Marginal'},
+  7: {dBm: -99,
+    condition: 'Marginal'},
+  8: {dBm: -97,
+    condition: 'Marginal'},
+  9: {dBm: -95,
+    condition: 'Marginal'},
+  10: {dBm: -93,
+    condition: 'OK'},
+  11: {dBm: -91,
+    condition: 'OK'},
+  12: {dBm: -89,
+    condition: 'OK'},
+  13: {dBm: -87,
+    condition: 'OK'},
+  14: {dBm: -85,
+    condition: 'OK'},
+  15: {dBm: -83,
+    condition: 'Good'},
+  16: {dBm: -81,
+    condition: 'Good'},
+  17: {dBm: -79,
+    condition: 'Good'},
+  18: {dBm: -77,
+    condition: 'Good'},
+  19: {dBm: -75,
+    condition: 'Good'},
+  20: {dBm: -73,
+    condition: 'Excellent'},
+  21: {dBm: -71,
+    condition: 'Excellent'},
+  22: {dBm: -69,
+    condition: 'Excellent'},
+  23: {dBm: -67,
+    condition: 'Excellent'},
+  24: {dBm: -65,
+    condition: 'Excellent'},
+  25: {dBm: -63,
+    condition: 'Excellent'},
+  26: {dBm: -61,
+    condition: 'Excellent'},
+  27: {dBm: -59,
+    condition: 'Excellent'},
+  28: {dBm: -57,
+    condition: 'Excellent'},
+  29: {dBm: -55,
+    condition: 'Excellent'},
+  30: {dBm: -53,
+    condition: 'Excellent'}
 }
